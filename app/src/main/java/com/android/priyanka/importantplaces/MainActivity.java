c2bd18da5f24db5e420d3e9bdd25bdbdcdac3118
priyanka-jha <26priyanka12@gmail.com>
2019-08-07 14:34:19 +0530

    Initial commit

---
+++ app/src/main/java/com/android/priyanka/importantplaces/MainActivity.java
@@ -0,0 +1,47 @@
+package com.android.priyanka.importantplaces;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.maps.model.LatLng;
+
+import java.util.ArrayList;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    static ArrayList<String> places = new ArrayList<>();
+    static ArrayList<LatLng> locations = new ArrayList<>();
+    static ArrayAdapter arrayAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ListView listView = findViewById(R.id.listPlaces);
+
+        places.add("Add a new Place");
+        locations.add(new LatLng(0, 0));
+
+        arrayAdapter = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_list_item_1, places);
+        listView.setAdapter(arrayAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent i = new Intent(MainActivity.this,MapsActivity.class);
+                i.putExtra("placeNumber",position);
+                startActivity(i);
+            }
+        });
+
+    }
+}
